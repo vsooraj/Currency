a67f87ef0221c7e2f1c5a61140a06398acaad016
sooraj.v <soorajv.c@ivedix.com>
2016-08-14 18:21:16 +0530

    currency Formatter 1

---
+++ GraphData.cs
@@ -0,0 +1,28 @@
+﻿namespace CuurencyFormat
+{
+    class GraphData
+    {
+        //List<object> dataGoal = new List<object>();
+        //List<object> dataActual = new List<object>();
+        //List<object> tempList = new List<object>();
+        //dataGoal = GetNewMRRSeriesDataGoal(companyId);
+        //dataActual = GetNewMRRSeriesDataActual(companyId);
+        //tempList = dataGoal.Concat(dataActual).ToList();
+        //var currency = APTCommon.Currency.GetCurrency(tempList);
+        //currency = currency == "H" ? "" : currency;
+        //    dataGoal = APTCommon.Currency.ConvertData(currency, dataGoal);
+        //    //List<string> data = GetNewMRRDataForXAxis(companyId);
+        //    dataActual = APTCommon.Currency.ConvertData(currency, dataActual);
+        //    string currencyFormat = "${c}" + currency;
+        //graphOptions.calculable = true;
+        //    graphOptions.color = color;
+
+        //    List<XAxis> xAxisList = new List<XAxis>();
+        //xAxisList.Add(new XAxis());
+        //    xAxisList[0].type = "category";
+        //    xAxisList[0].data = GetNewMRRDataForXAxis(companyId);
+        //graphOptions.xAxis = xAxisList;
+
+        //    axisLabel.formatter = "{value}" + currency;
+    }
+}
